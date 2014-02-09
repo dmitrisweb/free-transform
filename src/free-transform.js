8b6f34855d32aadda94f96f450a74ed41606ecf4
unknown <dmitry@BB-SYSTEM-0144.backbase.com>
2014-02-09 23:10:15 +0100

    first commit / add free-transform sources

---
+++ src/free-transform.js
@@ -0,0 +1,172 @@
+/*
+ *  ----------------------------------------------------------------
+ *  Version 0.1.0
+ *  Author : Dmitry Smirnov <dmitri.smirnov@gmail.com> (http://smirnovz.com)
+ *  Filename : free-transform.js
+ * ----------------------------------------------------------------
+ *  Title
+ * ----------------------------------------------------------------
+ *  Description:
+ *
+ * ----------------------------------------------------------------
+ *
+ * Usage :
+ *
+ *  initialization          : ft = new FreeTransform(HTMLElement)
+ *  apply transformation    : ft.to();
+ *
+ */
+
+
+(function(Matrix){
+    'use strict';
+
+    /* getVendorPrefix by http://davidwalsh.name/vendor-prefix */
+
+    var prefix = (function () {
+        var styles = window.getComputedStyle(document.documentElement, ''),
+            pre = (Array.prototype.slice
+                .call(styles)
+                .join('')
+                .match(/-(moz|webkit|ms)-/) || (styles.OLink === '' && ['', 'o'])
+            )[1],
+            dom = ('WebKit|Moz|MS|O').match(new RegExp('(' + pre + ')', 'i'))[1];
+        return {
+            dom: dom,
+            lowercase: pre,
+            css: '-' + pre + '-',
+            js: pre[0].toUpperCase() + pre.substr(1)
+        };
+    })();
+
+
+/*----------------------------------------------------------------*/
+/* Constructor
+/*----------------------------------------------------------------*/
+
+    var FreeTransform = function (el){
+
+        if(el && el.offsetWidth) {
+            var t = el.offsetTop,
+                l = el.offsetLeft,
+                w = el.offsetWidth,
+                h = el.offsetHeight;
+
+            this.el = el;
+            this.width = w;
+            this.height = h;
+            this.left = l;
+            this.top = t;
+            this.startPoints = [[0, 0], [0, h], [w, h], [w, 0]];
+
+            this.prefix = prefix.js;
+            this.el.style[this.prefix + 'TransformOrigin'] = '0 0';
+            this.el.style[this.prefix + 'Perspective'] = '0';
+
+            return this;
+        }
+    };
+
+    var FT = FreeTransform.prototype;
+
+
+/*----------------------------------------------------------------*/
+/* Private
+/*----------------------------------------------------------------*/
+
+    var getValues = function(points, startPoints){
+        var i = 0,
+            len = points.length;
+        for (; i < len ; i++) {
+            points[i] = [
+                points[i][0] + startPoints[i][0],
+                points[i][1] + startPoints[i][1]
+            ];
+        }
+
+        return points;
+    };
+
+
+
+/*----------------------------------------------------------------*/
+/* Public
+/*----------------------------------------------------------------*/
+
+    FT.prefix = prefix;
+
+    FT.getTransformationMatrix = function(points){
+        var m1 = this.startPoints,
+            i = 0, len = 4,
+            aM = [], aL = [],
+            M, Mi, X,
+            m2 = getValues(points, this.startPoints);
+
+        // do magic
+        for(;i<len;i++){
+            aM.push([m1[i][0], m1[i][1], 1, 0, 0, 0, -m1[i][0] * m2[i][0], -m1[i][1] * m2[i][0]]);
+            aM.push([0, 0, 0, m1[i][0], m1[i][1], 1, -m1[i][0] * m2[i][1], -m1[i][1] * m2[i][1]]);
+            aL.push([m2[i][0]]);
+            aL.push([m2[i][1]]);
+        }
+
+        // create matrix
+        M = new Matrix(aM);
+
+        // inverse matrix
+        Mi = M.inverse();
+
+        // multiply
+        X = Mi ? Mi.multiply(new Matrix(aL)) : null;
+
+        return X;
+    };
+
+
+    FT.getMatrixValues = function(points){
+        var tM = this.getTransformationMatrix(points),
+            t = function (i) {
+                var val = tM ? tM.e(i, 1) : 0;
+                return val;
+            },
+            m = [
+                t(1), t(4), 0, t(7),
+                t(2), t(5), 0, t(8),
+                0, 0, 1, 0,
+                t(3), t(6), 0, 1
+            ];
+        return m;
+    };
+
+
+    FT.getCssValue = function(m){
+
+        if(!(m && m.length)) return;
+
+        // normalize values for CSS
+        m = m.map(function(val){
+            return parseFloat(val.toFixed(6)).toString();
+        });
+
+        return 'matrix3d(' + (m.join(', ')) + ')';
+    };
+
+
+    FT.to = function(points){
+        var m = this.getMatrixValues(points),
+            s = this.getCssValue(m);
+
+        this.el.style[this.prefix + 'Transform'] = s;
+        return s;
+    };
+
+
+    FT.reset = function(){
+        this.el.style[this.prefix + 'Transform'] = null;
+    };
+
+
+    window.FreeTransform = FreeTransform;
+
+})(window.$M);
+
---
+++ src/plugins/controls.js
@@ -0,0 +1,176 @@
+(function(Utils){
+    'use strict';
+
+    if (!this || !Utils) return;
+
+/*----------------------------------------------------------------*/
+/* Private
+/*----------------------------------------------------------------*/
+
+    var This, oParams = {};
+
+    var addControls = function(){
+        var i = 0, s = '', top, left,
+            t = [0, 1, 2, 2, 2, 1, 0, 0],
+            point,
+            offset = 5;
+
+        This.controls = document.createElement('div');
+        This.controls.className = 'ft-controls';
+
+        // build html for controls
+        for(; i < 8; i++){
+            top = (t[i] * This.height) / 2 + This.top;
+            left = (t[(i+6) % 8] * This.width) / 2 + This.left;
+
+            s += '<div class="ft-' + i + '" draggable="false" style="top: '+
+                (top - offset) +'px; left: '+
+                (left - offset) +'px" title="' + i + '"></div>';
+        }
+
+        This.controls.innerHTML = s;
+        This.el.parentNode.insertBefore(This.controls, This.el);
+        This.points = This.controls.childNodes;
+
+
+        for(i = 0; i < 8; i++){
+            point = This.points[i];
+
+            // Set initial offset of controls
+            point.setAttributeNS(null, 'iLeft', point.offsetLeft);
+            point.setAttributeNS(null, 'iTop',  point.offsetTop);
+
+            // Enable dnd
+            Utils.dnd.initElement(This.points[i]);
+        }
+    };
+
+
+    var addEvents = function(){
+        var events = {
+                dragstart2: handleDragStart,
+                drag2: handleDrag
+            },
+
+            handler = function(e){
+                //console.log(e);
+                var el = e.target,
+                    index = parseInt(el.className.substring(3), 10);
+
+                //console.log(index);
+
+                events[e.type](e, el, index);
+
+            };
+
+        This.controls.addEventListener('dragstart2', handler, false);
+        This.controls.addEventListener('drag2', handler, false);
+
+    };
+
+
+    var handleDragStart = function(e, el, index){
+
+        var points = This.points,
+            getPoint = function(i){
+                var v = points[(index + i) % 8];
+                return [v, v.offsetLeft, v.offsetTop];
+            };
+
+        This.dragPoints = {
+            oneBefore   : getPoint(7),
+            oneAfter    : getPoint(1),
+            twoBefore   : getPoint(6),
+            twoAfter    : getPoint(2),
+            threeBefore : getPoint(5),
+            threeAfter  : getPoint(3)
+        };
+    };
+
+
+    var handleDrag = function(e, el, index){
+
+        if (e.preventDefault) e.preventDefault();
+        if (e.stopPropogation) e.stopPropogation();
+
+        var point,
+            newPoints = [],
+            points = This.points,
+            i = 0, len = points.length/2, s;
+
+        var setVal = function(el, b, c){
+            el.style.top =  parseInt(((b.offsetTop + c.offsetTop)/2), 10) + 'px';
+            el.style.left =  parseInt(((b.offsetLeft + c.offsetLeft)/2), 10) + 'px';
+        };
+
+        var oneBefore   = points[(index+7) % 8],
+            oneAfter    = points[(index+1) % 8],
+            twoBefore   = points[(index+6) % 8],
+            twoAfter    = points[(index+2) % 8],
+            threeBefore = points[(index+5) % 8],
+            threeAfter  = points[(index+3) % 8];
+
+        if(index % 2 === 0) {
+            // drag corner
+
+            setVal(oneBefore, twoBefore, el);
+            setVal(oneAfter, twoAfter, el);
+
+        } else {
+            // drag side
+
+            setCornerPosition(This.dragPoints.oneBefore, e.data.dY, e.data.dX);
+            setCornerPosition(This.dragPoints.oneAfter, e.data.dY, e.data.dX);
+
+            setVal(twoBefore, oneBefore, threeBefore);
+            setVal(twoAfter, oneAfter, threeAfter);
+        }
+
+
+        // put new coordinates
+        for(; i < len; i++){
+            point = points[i*2];
+            newPoints.push([
+                point.offsetLeft - point.getAttributeNS(null, 'iLeft'),
+                point.offsetTop - point.getAttributeNS(null, 'iTop')
+            ]);
+        }
+
+        s = This.to(newPoints);
+        if(oParams.el) oParams.el.innerHTML = s;
+        return true;
+    };
+
+
+    var setCornerPosition = function(arr, t, l){
+        var el = arr[0],
+            left = arr[1],
+            top = arr[2];
+
+        el.style.top    = (top  + t) + 'px';
+        el.style.left   = (left + l) + 'px';
+    };
+
+
+
+
+/*----------------------------------------------------------------*/
+/* Public
+/*----------------------------------------------------------------*/
+
+    // extend FreeTransform
+    this.prototype.enableControls = function(params){
+        This = this;
+        oParams = params;
+        addControls();
+        addEvents();
+
+        return this;
+    };
+
+    // TODO: removeControls
+
+
+}).call(window.FreeTransform, window.utils);
+
+
